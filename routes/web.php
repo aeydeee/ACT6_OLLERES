70e026023d4745e0e6593237f3c69d8dcc36b6b4
aeydeee <aeydeee@gmail.com>
2024-04-03 17:37:01 +0800

    Add index and show posts

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('post.index');
+});
+
+Route::get('/post/{post}', function ($post) {
+    return view('post.show', [
+        'post' => $post
+    ]);
 });
